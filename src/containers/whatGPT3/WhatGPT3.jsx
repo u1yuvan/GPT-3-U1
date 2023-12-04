4cb8d05e4e93dcc6cde260c52d7f9afaafae3bde
u1yuvan <yuvanraj700@gmail.com>
2023-12-04 18:57:30 +0530

    Brand has beeen implemented going further in What is GPT-3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './whatGPT3.css'
+import { Feature } from '../../components';
+
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-features'>
+        <Feature title='What is GPT-3' text="
+GPT-3 AI, developed by OpenAI, is a cutting-edge language model with 175 billion parameters, making it one of the largest and most powerful in existence. This versatile model excels in natural language understanding and generation, enabling applications like chatbots, text completion, and language translation. Leveraging deep learning, GPT-3 processes and generates human-like text based on input, making it a key tool for various natural language processing tasks. Explore the capabilities of GPT-3 for advanced language-related applications on the OpenAI website." />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+          <Feature title="Chatbots" text="
+Chatbots are AI-powered programs designed to engage in conversation and provide automated responses to users."/>
+          <Feature  title="Knowledgebase" text="It is a centralized repository of information that serves as a reference and resource for users to access relevant data, documentation, and solutions."/>
+          <Feature title="Education" text="This websites utilize artificial intelligence to deliver personalized and adaptive learning experiences, enhancing comprehension and catering to individual needs."/>
+      </div>
+    </div>
   )
 }
 
